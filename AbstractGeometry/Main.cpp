fc22ba94c96603a224f8397e0b8c395b96814334
Admin <Admin@ADMIN>
2023-06-11 09:06:01 +0300

    Encoding

---
+++ AbstractGeometry/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+	virtual double get_are()const = 0;
+	virtual double get_perimetr()const = 0;
+	virtual void draw()const = 0;
+};
+class Sguare : public Shape
+{
+	double side;
+public:
+	Sguare(double side)
+	{
+		this->side = side;
+	}
+	~Sguare() {}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+
+}
+
